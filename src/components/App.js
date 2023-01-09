151238d3bebcd25f0621dbc2c7e9f19856373e24
Riyarajns <108589846+Riyarajns@users.noreply.github.com>
2023-01-10 01:25:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,17 @@
-import React , {useState,useEffect} from 'react'
-import '../styles/App.css';
-import List from "./List";
-const App = () => {
-  //code here
-  const [name, setname] = useState("");
-  const[list, setlist] = useState([]);
-
-  const handleChange =(e) => {
-    setname(e.target.value);
-  }
-  const handleOnClick =()=>{
-    // setlist([...name, list])
-    // list.push(name)
-    setname("");
-    setlist([...list, name])
-
-  }
-
-  
-  return (
-    <div id="main">
-       <input id="input" onChange={handleChange} />
-      <button id="button" value = {name} onClick={handleOnClick}>Click</button>
-       <ul id="list">
-      <List listx={list} />
+import React from 'react';
+import ListItems from './ListItems';
+//code here
+ const List = ({listx}) =>{
+  return(
+    
+    <>
+      <ul id='list'>
+        {listx.map((list  => <ListItems valuex ={list}/>))}
       </ul>
-    </div>
+    </>
+    
+  
   )
 }
 
-
-export default App;
+export default List;
